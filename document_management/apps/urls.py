ead95b9ed7659ae73529048738ba7f68ec5938a2
simonnicholov <nicholovsimon@gmail.com>
2019-05-08 23:01:04 +0700

    added halaman location

--- document_management/apps/urls.py
+++ document_management/apps/urls.py
@@ -7,6 +7,7 @@ app_name = 'apps'
 urlpatterns = [
     # namespace urls
     path('', include('document_management.apps.users.urls', namespace="users")),
+    path('location/', include('document_management.apps.locations.urls', namespace="locations")),
 
     # specific urls
     path('dashboard/', views.dashboard, name="dashboard"),
