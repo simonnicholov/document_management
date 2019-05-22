4223f1076a8a0517c7eaf629bb4015162de46c26
simonnicholov <nicholovsimon@gmail.com>
2019-05-22 21:47:20 +0700

    added halaman users

--- document_management/apps/users/urls.py
+++ document_management/apps/users/urls.py
@@ -4,5 +4,14 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
+    # External
     path('login/', views.login_view, name="login_view"),
+
+    # Internal
+    path('', views.index, name="index"),
+    path('add/', views.add, name="add"),
+    path('<int:id>/edit/', views.edit, name="edit"),
+    path('<int:id>/delete/', views.delete, name="delete"),
+    path('<int:id>/details/', views.details, name="details"),
+    path('<int:id>/reset-password/', views.reset_password, name="reset_password"),
 ]
