f03d800e2539f7809f2a39ba98cb177f1d9e330d
simonnicholov <nicholovsimon@gmail.com>
2019-07-29 23:41:22 +0700

    added midtest

--- document_management/apps/views.py
+++ document_management/apps/views.py
@@ -6,3 +6,17 @@ def dashboard(request):
         'title': 'Dashboard'
     }
     return render(request, 'dashboard.html', context)
+
+
+def permission_requests(request):
+    context = {
+        'title': 'Permission Requests'
+    }
+    return render(request, 'permission_requests.html', context)
+
+
+def approval_requests(request):
+    context = {
+        'title': 'Approval Requests'
+    }
+    return render(request, 'approval_requests.html', context)
