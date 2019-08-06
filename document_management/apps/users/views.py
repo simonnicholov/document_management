acd0ce137b75ebab8dd2f60ee4c4b076ba39d9e2
simonnicholov <nicholovsimon@gmail.com>
2019-08-07 00:35:19 +0700

    added modify urls

--- document_management/apps/users/views.py
+++ document_management/apps/users/views.py
@@ -1,26 +1,6 @@
-from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 
-# region external
-def login_view(request):
-    form = AuthenticationForm(data=request.POST or None)
-    if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        if 'next' in request.POST:
-            return redirect(request.POST.get('next'))
-        else:
-            return redirect('backoffice:dashboard')
-    context = {
-        'title': 'Login',
-        'form': form
-    }
-    return render(request, 'login.html', context)
-
-
-# region internal
 def index(request):
     context = {
         'title': 'User'
