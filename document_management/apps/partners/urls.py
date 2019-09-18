f721bad741b527f012054a6516c7514a758c68dd
simonnicholov <nicholovsimon@gmail.com>
2019-09-18 21:06:18 +0700

    add partner index

--- document_management/apps/partners/urls.py
+++ document_management/apps/partners/urls.py
@@ -6,6 +6,7 @@ app_name = 'partners'
 urlpatterns = [
     path('', views.index, name="index"),
     path('add/', views.add, name="add"),
+    # for now, this is unavaiable at phase one
     path('search/', views.search, name="search"),
     path('<int:id>/edit/', views.edit, name="edit"),
     path('<int:id>/delete/', views.delete, name="delete"),
