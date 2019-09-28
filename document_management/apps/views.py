5b38f6aaf7dabdc1c7a8521b88cf47e6f4889355
simonnicholov <nicholovsimon@gmail.com>
2019-09-28 11:31:07 +0700

    add dashboard legal

--- document_management/apps/views.py
+++ document_management/apps/views.py
@@ -1,10 +1,17 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
+from django.utils import timezone
 
+from document_management.apps.addendums.models import Addendum
+from document_management.apps.documents.models import Document
+from document_management.apps.permission_requests.models import PermissionRequest
 from document_management.core.decorators import legal_required, user_required
 
 from .forms import PasswordChangeForm
@@ -43,8 +50,42 @@ def logout_view(request):
 
 @legal_required
 def dashboard_legal(request):
+    document = Document.objects.filter(is_active=True)
+    total_contract = document.filter(group=settings.GROUP_CONTRACT).count()
+    total_mou = document.filter(group=settings.GROUP_MOU).count()
+    total_official_record = document.filter(group=settings.GROUP_OFFICIAL_RECORD).count()
+    total_company_regulation = document.filter(group=settings.GROUP_COMPANY_REGULATION).count()
+
+    total_addendum = Addendum.objects.filter(is_active=True).count()
+    total_approval = PermissionRequest.objects.all().count()
+
+    today = datetime.now().date()
+    next_month = today + relativedelta(months=+1)
+    three_month = today + relativedelta(months=+3)
+    print(today)
+    print(next_month)
+    print(three_month)
+
+    # 3 months expiration
+    three_month_lists = document.filter(expired_date__range=(today, three_month))
+    print(three_month_lists)
+
+    # monthly expiration
+    next_month_lists = document.filter(expired_date__range=(today, next_month))
+    print(next_month_lists)
+
     context = {
-        'title': 'Dashboard'
+        'title': 'Dashboard',
+        'total_contract': total_contract,
+        'total_mou': total_mou,
+        'total_official_record': total_official_record,
+        'total_company_regulation': total_company_regulation,
+        'total_addendum': total_addendum,
+        'total_approval': total_approval,
+        'total_three_months': three_month_lists.count(),
+        'total_next_month': next_month_lists.count(),
+        'three_month_lists': three_month_lists,
+        'next_month_lists': next_month_lists
     }
     return render(request, 'dashboard_legal.html', context)
 
