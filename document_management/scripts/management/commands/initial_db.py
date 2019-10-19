d470bb2ce3e5a956a5d242e4499c395e1d47f5dc
simonnicholov <nicholovsimon@gmail.com>
2019-10-19 16:05:12 +0700

    add remove blank null true

--- document_management/scripts/management/commands/initial_db.py
+++ document_management/scripts/management/commands/initial_db.py
@@ -94,61 +94,52 @@ class Command(LoggingBaseCommand):
                      subject='CONTRACT-001 Subject', signature_date='2019-09-18',
                      effective_date='2019-09-19', expired_date='2019-09-30', amount=50000000,
                      group=Document.GROUP.contract, category=Document.CATEGORY.construction,
-                     type=Document.TYPE.private, job_specification='Pembuatan keramik',
-                     retention_period=90),
+                     type=Document.TYPE.private, job_specification='Pembuatan keramik'),
             Document(partner=partner2, location=location2, number='CONTRACT-002',
                      subject='CONTRACT-002 Subject', signature_date='2019-09-19',
                      effective_date='2019-09-20', expired_date='2019-10-1', amount=75000000,
                      group=Document.GROUP.contract, category=Document.CATEGORY.property,
-                     type=Document.TYPE.public, job_specification='Pembuatan bata',
-                     retention_period=45),
+                     type=Document.TYPE.public, job_specification='Pembuatan bata'),
             Document(partner=partner3, location=location3, number='CONTRACT-003',
                      subject='CONTRACT-003 Subject', signature_date='2019-09-19',
                      effective_date='2019-09-22', expired_date='2019-10-3', amount=35500000,
                      group=Document.GROUP.contract, category=Document.CATEGORY.other,
-                     type=Document.TYPE.private, job_specification='Pembuatan dinding kayu',
-                     retention_period=75),
+                     type=Document.TYPE.private, job_specification='Pembuatan dinding kayu'),
 
             # mou
             Document(partner=partner4, location=location3, number='MOU-001',
                      subject='MOU-001 Subject', signature_date='2019-09-15',
                      effective_date='2019-09-17', expired_date='2019-09-25', amount=50000000,
                      group=Document.GROUP.mou, category=Document.CATEGORY.construction,
-                     type=Document.TYPE.private, job_specification='Pemasangan alumunium',
-                     retention_period=120),
+                     type=Document.TYPE.private, job_specification='Pemasangan alumunium'),
             Document(partner=partner2, location=location2, number='MOU-002',
                      subject='MOU-002 Subject', signature_date='2019-09-19',
                      effective_date='2019-09-23', expired_date='2019-10-1', amount=75000000,
                      group=Document.GROUP.mou, category=Document.CATEGORY.property,
-                     type=Document.TYPE.public, job_specification='Pembuatan bata',
-                     retention_period=45),
+                     type=Document.TYPE.public, job_specification='Pembuatan bata'),
             Document(partner=partner3, location=location1, number='MOU-003',
                      subject='MOU-003 Subject', signature_date='2019-09-17',
                      effective_date='2019-09-25', expired_date='2019-09-27', amount=35500000,
                      group=Document.GROUP.mou, category=Document.CATEGORY.other,
-                     type=Document.TYPE.private, job_specification='Pembuatan pipa pvc',
-                     retention_period=25),
+                     type=Document.TYPE.private, job_specification='Pembuatan pipa pvc'),
 
 
-            # official record
-            Document(partner=partner1, location=location3, number='OFFICIAL-RECORD-001',
+            # official record unrelated
+            Document(partner=partner1, location=location2, number='OFFICIAL-RECORD-001',
                      subject='OFFICIAL-RECORD-001 Subject', signature_date='2019-09-15',
                      effective_date='2019-09-17', expired_date='2019-09-25', amount=50000000,
                      group=Document.GROUP.official_record, category=Document.CATEGORY.construction,
-                     type=Document.TYPE.private, job_specification='Pemasangan alumunium',
-                     retention_period=120),
-            Document(partner=partner3, location=location2, number='OFFICIAL-RECORD-002',
+                     type=Document.TYPE.private, job_specification='Pemasangan alumunium'),
+            Document(partner=partner2, location=location3, number='OFFICIAL-RECORD-002',
                      subject='OFFICIAL-RECORD-002 Subject', signature_date='2019-09-14',
                      effective_date='2019-09-15', expired_date='2019-09-25', amount=25500000,
                      group=Document.GROUP.official_record, category=Document.CATEGORY.property,
-                     type=Document.TYPE.public, job_specification='Pembuatan batako',
-                     retention_period=37),
-            Document(partner=partner2, location=location1, number='OFFICIAL-RECORD-003',
+                     type=Document.TYPE.public, job_specification='Pembuatan batako'),
+            Document(partner=partner3, location=location1, number='OFFICIAL-RECORD-003',
                      subject='OFFICIAL-RECORD-003 Subject', signature_date='2019-09-12',
                      effective_date='2019-09-13', expired_date='2019-09-20', amount=32500000,
                      group=Document.GROUP.official_record, category=Document.CATEGORY.other,
-                     type=Document.TYPE.private, job_specification='Pembuatan besi plamir',
-                     retention_period=15),
+                     type=Document.TYPE.private, job_specification='Pembuatan besi plamir'),
 
             # company regulation
             Document(number='COMPANY-REGULATION-001', subject='COMPANY-REGULATION-001 Subject',
@@ -166,14 +157,11 @@ class Command(LoggingBaseCommand):
 
         ])
 
-        document1 = Document.objects.get(id=21)
-        document2 = Document.objects.get(id=22)
+        document1 = Document.objects.get(id=1)
+        document2 = Document.objects.get(id=2)
 
-        document4 = Document.objects.get(id=24)
-        document5 = Document.objects.get(id=25)
-
-        document7 = Document.objects.get(id=27)
-        document8 = Document.objects.get(id=28)
+        document4 = Document.objects.get(id=4)
+        document5 = Document.objects.get(id=5)
 
         Addendum.objects.bulk_create([
 
@@ -193,6 +181,8 @@ class Command(LoggingBaseCommand):
                      effective_date='2019-09-21', expired_date='2019-09-30',
                      amount=99250000, job_specification='Buat addendum contract 002-1',
                      retention_period=90),
+            Addendum(document=document2, number='CONTRACT-002-ADDENDUM-002',
+                     subject='CONTRACT-002-ADDENDUM-002 Subject'),
 
             # addendum mou
             Addendum(document=document4, number='MOU-001-ADDENDUM-001',
@@ -200,6 +190,8 @@ class Command(LoggingBaseCommand):
                      effective_date='2019-09-20', expired_date='2019-09-25',
                      amount=78500000, job_specification='Buat addendum mou 001',
                      retention_period=80),
+            Addendum(document=document4, number='MOU-001-ADDENDUM-002',
+                     subject='MOU-001-ADDENDUM-002 Subject'),
             Addendum(document=document5, number='MOU-002-ADDENDUM-001',
                      subject='MOU-002-ADDENDUM-001 Subject', signature_date='2019-09-19',
                      effective_date='2019-09-23', expired_date='2019-10-1',
@@ -235,6 +227,8 @@ class Command(LoggingBaseCommand):
                            effective_date='2019-09-21', expired_date='2019-09-30',
                            amount=99250000, job_specification='Buat official record contract 002-1',
                            retention_period=90),
+            OfficialRecord(document=document2, number='CONTRACT-002-OFFICIAL-RECORD-002',
+                           subject='CONTRACT-002-OFFICIAL-RECORD-002 Subject'),
 
             # official record mou
             OfficialRecord(document=document4, number='MOU-001-OFFICIAL-RECORD-001',
@@ -242,6 +236,8 @@ class Command(LoggingBaseCommand):
                            effective_date='2019-09-20', expired_date='2019-09-25',
                            amount=78500000, job_specification='Buat official record mou 001',
                            retention_period=80),
+            OfficialRecord(document=document4, number='MOU-001-OFFICIAL-RECORD-002',
+                           subject='MOU-001-OFFICIAL-RECORD-002 Subject'),
             OfficialRecord(document=document5, number='MOU-002-OFFICIAL-RECORD-001',
                            subject='MOU-002-OFFICIAL-RECORD-001 Subject', signature_date='2019-09-19',
                            effective_date='2019-09-23', expired_date='2019-10-1',
